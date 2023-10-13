069c77be2eb2120481899cedbb687a8926269fdc
Stian Frøystein <stianfroy@gmail.com>
2023-10-13 11:33:31 +0900

    create working composite resources

--- cluster-install.sh
+++ cluster-install.sh
@@ -13,3 +13,28 @@ kubectl wait --namespace crossplane-system \
              --for=condition=ready pod \
              --selector=app.kubernetes.io/instance=crossplane \
              --timeout=90s
+
+cat << EOF | kubectl apply -f -
+apiVersion: pkg.crossplane.io/v1
+kind: Provider
+metadata:
+  name: provider-kubernetes
+spec:
+  package: "crossplanecontrib/provider-kubernetes:main"
+EOF
+
+echo "Sleeping for 10 seconds"
+sleep 10
+
+SA=$(kubectl -n crossplane-system get sa -o name | grep provider-kubernetes | sed -e 's|serviceaccount\/|crossplane-system:|g')
+kubectl create clusterrolebinding provider-kubernetes-admin-binding --clusterrole cluster-admin --serviceaccount="${SA}"
+
+cat << EOF | kubectl apply -f -
+apiVersion: kubernetes.crossplane.io/v1alpha1
+kind: ProviderConfig
+metadata:
+  name: kubernetes-provider
+spec:
+  credentials:
+    source: InjectedIdentity
+EOF
--- install-resources.sh
+++ install-resources.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
-# Applying the Composite Resource Definition (XRD)
-kubectl apply -f xrd/webapplication-xrd.yaml
-kubectl get compositeresourcedefinition.apiextensions.crossplane.io/webapplications.example.com
-
-# Waiting for the XRD to be registered
-echo "Waiting for XRD to be registered..."
-sleep 10  # Adjust this value as needed
-
-# Applying the Composition
-kubectl apply -f compositions/webapplication-composition.yaml
-
-# Waiting for the Composition to be registered
-echo "Waiting for Composition to be registered..."
-sleep 10  # Adjust this value as needed
-
-# Applying the Composite Resource (XR)
-kubectl apply -f xr/test-app.yaml
-
-echo "Resources have been applied to the cluster."
+kubectl apply -f compositions/webapp.yaml
+kubectl apply -f xrds/xwebapp.yaml
+kubectl apply -f claims/example.yaml
+kubectl apply -f claims/example-qa.yaml
+kubectl apply -f claims/example-prod.yaml
